a364ae8ec9ad6e8c34cf06b91c2fe2e2581ffdfb
wojciech <wojciechstachnik09@gmail.com>
2020-04-10 13:33:20 -0400

    remove ngrok

--- server/node/config.js
+++ server/node/config.js
@@ -69,13 +69,13 @@ module.exports = {
   ],
 
   // Server port.
-  port: process.env.PORT || 8000,
+  port: process.env.PORT || 5000,
 
   // Tunnel to serve the app over HTTPS and be able to receive webhooks locally.
   // Optionally, if you have a paid ngrok account, you can specify your `subdomain`
   // and `authtoken` in your `.env` file to use it.
   ngrok: {
-    enabled: process.env.NODE_ENV !== 'production',
+    enabled: false,
     port: process.env.PORT || 8000,
     subdomain: process.env.NGROK_SUBDOMAIN,
     authtoken: process.env.NGROK_AUTHTOKEN,
